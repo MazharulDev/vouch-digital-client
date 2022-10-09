9f9e0cddb1109e3d308a24c8bd1ebb04df202726
Md Mazharul Islam <mazharulislam.dev@gmail.com>
2022-10-09 19:13:21 +0600

    view client loading spinner added

--- src/components/home/Clients/ViewClients.js
+++ src/components/home/Clients/ViewClients.js
@@ -5,12 +5,15 @@ import { useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
 import Client from './Client';
 import axios from 'axios';
+import Loading from '../../../shared/Loading';
 
 const ViewClients = () => {
+    const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
     const [clients, setClients] = useState([]);
     useEffect(() => {
         const getEmail = async () => {
+            setLoading(true)
             const url = `https://vouch-digital.onrender.com/clients`
             try {
                 const { data } = await axios.get(url, {
@@ -18,6 +21,7 @@ const ViewClients = () => {
                         authorization: `Bearer ${localStorage.getItem('accessToken')}`
                     }
                 });
+                setLoading(false)
                 setClients(data)
 
             }
@@ -31,45 +35,49 @@ const ViewClients = () => {
         getEmail()
     }, [navigate])
     return (
-        <div>
-            <h2 className='text-2xl font-bold my-5 px-3'>View client</h2>
-            <p className='px-3 font '>Client Master / <span className='text-gray-400'>View Clients</span></p>
-            <div className='flex justify-start items-center mx-5'>
-                <AiOutlineSearch className='absolute pl-2 text-3xl' />
-                <input className='my-4 py-2 px-1 border rounded-full pl-8' type="text" name="" placeholder='Search Modules'></input>
-            </div>
-            <div>
+        <>
+            {
+                loading ? <Loading /> : <div>
+                    <h2 className='text-2xl font-bold my-5 px-3'>View client</h2>
+                    <p className='px-3 font '>Client Master / <span className='text-gray-400'>View Clients</span></p>
+                    <div className='flex justify-start items-center mx-5'>
+                        <AiOutlineSearch className='absolute pl-2 text-3xl' />
+                        <input className='my-4 py-2 px-1 border rounded-full pl-8' type="text" name="" placeholder='Search Modules'></input>
+                    </div>
+                    <div>
 
-                <table className="table table-compact w-full">
-                    <thead>
-                        <tr>
-                            <th>Company Name</th>
-                            <th>Email Address</th>
-                            <th>Phone No.</th>
-                            <th>Contact Person</th>
-                            <th>Facilitator</th>
-                            <th>Sites</th>
-                            <th>Tenants</th>
-                            <th>Tenants Groups</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
+                        <table className="table table-compact w-full">
+                            <thead>
+                                <tr>
+                                    <th>Company Name</th>
+                                    <th>Email Address</th>
+                                    <th>Phone No.</th>
+                                    <th>Contact Person</th>
+                                    <th>Facilitator</th>
+                                    <th>Sites</th>
+                                    <th>Tenants</th>
+                                    <th>Tenants Groups</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
 
-                    <tbody>
-                        {
-                            clients.map(client =>
-                                <Client client={client} key={client._id} />
-                            )
-                        }
-                    </tbody>
-
-
-
-                </table>
+                            <tbody>
+                                {
+                                    clients?.map(client =>
+                                        <Client client={client} key={client._id} />
+                                    )
+                                }
+                            </tbody>
 
 
-            </div>
-        </div>
+
+                        </table>
+
+
+                    </div>
+                </div>
+            }
+        </>
     );
 };
 
